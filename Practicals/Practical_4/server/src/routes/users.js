5d25b85b4512ae9a5f7efd00f62d337df6450ef0
Norzin Wangmo <norzinwangmo@macbook.local>
2026-05-02 23:39:15 +0600

    prisma check

--- Practicals/Practical_4/server/src/routes/users.js
+++ Practicals/Practical_4/server/src/routes/users.js
@@ -1,18 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const { protect } = require('../middleware/auth');
+const userController = require("../controllers/userController");
+const { protect } = require("../middleware/auth");
 
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.post('/register', userController.createUser);
-router.post('/login', userController.loginUser);
-router.put('/:id', protect, userController.updateUser);
-router.delete('/:id', protect, userController.deleteUser);
-router.get('/:id/videos', userController.getUserVideos);
-router.get('/:id/followers', userController.getUserFollowers);
-router.get('/:id/following', userController.getUserFollowing);
-router.post('/:id/follow', protect, userController.followUser);
-router.delete('/:id/follow', protect, userController.unfollowUser);
+// public routes - no token needed
+router.post("/register", userController.createUser);
+router.post("/login", userController.loginUser);
+
+// other routes
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.put("/:id", protect, userController.updateUser);
+router.delete("/:id", protect, userController.deleteUser);
 
 module.exports = router;
